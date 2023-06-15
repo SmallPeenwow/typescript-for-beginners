06b676c41c0c9bb9e324c4534f63a292704dadb9
SmallPeenwow <bowdencamryn@gmail.com>
2023-06-15 14:06:09 +0200

    TypeScript React Props Done

--- react/src/components/postCard/PostCard.tsx
+++ react/src/components/postCard/PostCard.tsx
@@ -1,7 +1,13 @@
+import { PostProps } from '@/types/PostProps.type';
 import React from 'react';
 
-const PostCard = () => {
-	return <div>PostCard</div>;
+const PostCard = ({ title, body }: PostProps) => {
+	return (
+		<div className=''>
+			<h1>{title}</h1>
+			<p>{body}</p>
+		</div>
+	);
 };
 
 export default PostCard;
--- react/src/components/postList/PostList.tsx
+++ react/src/components/postList/PostList.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import PostCard from '../postCard/PostCard';
+import { PostProps } from '@/types/PostProps.type';
+
+async function getData() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch data');
+	}
+
+	return res.json();
+}
 
 const PostList = async () => {
-	return <div className=''>PostList</div>;
+	const data: PostProps[] = await getData();
+
+	return (
+		<div className='postList'>
+			{/* <PostCard title='post title' body='post desc' /> */}
+			{data.map((post) => (
+				<PostCard key={post.id} {...post} />
+			))}
+		</div>
+	);
 };
 
 export default PostList;
