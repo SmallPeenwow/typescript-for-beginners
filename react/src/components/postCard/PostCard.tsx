47bfdf36895b0461e875738a1d917db3f91fd11f
SmallPeenwow <bowdencamryn@gmail.com>
2023-06-15 14:22:56 +0200

    TypeScript React Props Done

--- react/src/components/postCard/PostCard.tsx
+++ react/src/components/postCard/PostCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const PostCard = ({ title, body }: PostProps) => {
 	return (
-		<div className=''>
+		<div className='postCard'>
 			<h1>{title}</h1>
 			<p>{body}</p>
 		</div>
