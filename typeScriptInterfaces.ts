feb7890da5e68ec33943f7af50fe6f9d127a7eda
SmallPeenwow <bowdencamryn@gmail.com>
2023-06-15 12:43:45 +0200

    TypeScript Interfaces Done

---
+++ typeScriptInterfaces.ts
@@ -0,0 +1,22 @@
+interface IUser {
+	username: string;
+	email: string;
+	age: number;
+}
+
+interface IEmployee extends IUser {
+	employeeId: number;
+}
+
+const emp: IEmployee = {
+	username: 'tom',
+	email: 'tom@gmail.com',
+	age: 43,
+	employeeId: 1,
+};
+
+const client: IUser = {
+	username: 'tom',
+	email: 'tom@gmail.com',
+	age: 43,
+};
