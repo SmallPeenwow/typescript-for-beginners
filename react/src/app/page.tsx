4501fd2819c7c0857e84cc79c7951c826f7e9041
SmallPeenwow <bowdencamryn@gmail.com>
2023-06-16 15:17:57 +0200

    TypeScript React UseRef Done

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -5,6 +5,7 @@ import ChildrenPropExample from './2-children-prop-example/page';
 import EventExample from './3-event-example/page';
 import UseStateExample from './4-usestate-example/page';
 import UseContextExample from './5-usecontext-example/page';
+import UseRefExample from './6-useref-example/page';
 
 export default function Home() {
 	return (
@@ -93,6 +94,7 @@ export default function Home() {
 			<EventExample />
 			<UseStateExample />
 			<UseContextExample />
+			<UseRefExample />
 		</>
 	);
 }
